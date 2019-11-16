12b9456cfe90519e6ce036be5d7e12b108c31e63
Mike Wittenauer <mikewittenauer@me.com>
2019-11-16 08:38:33 -0500

    Stub out URL CRUD endpoints

---
+++ UrlShortener/Controllers/UrlController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UrlController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
