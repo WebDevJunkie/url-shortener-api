500cc53818cd4d9f4b700b78c1e3f9338d3a09f8
Mike Wittenauer <mikewittenauer@me.com>
2019-11-16 17:17:47 -0500

    Finish basic CRUD for URLs

--- UrlShortener/Controllers/UrlController.cs
+++ UrlShortener/Controllers/UrlController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using UrlShortener.Context;
+using UrlShortener.Dto;
+using UrlShortener.Models;
 
 namespace UrlShortener.Controllers
 {
@@ -6,27 +10,55 @@ namespace UrlShortener.Controllers
     [Route("[controller]")]
     public class UrlController : ControllerBase
     {
+        private UrlContext _context;
+
+        public UrlController(UrlContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok();
+            var urls = _context.Urls.ToList();
+            return Ok(urls);
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            return Ok();
+            var url = _context.Urls.Find(id);
+
+            if (url == null)
+                return NotFound();
+
+            return Ok(url);
         }
 
         [HttpPost]
-        public IActionResult Create()
+        public IActionResult Create([FromBody] UrlDto urlDto)
         {
-            return Ok();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var url = new Url() { Description = urlDto.Description, Redirect = urlDto.Redirect };
+            _context.Urls.Add(url);
+            _context.SaveChanges();
+
+            return Ok(url);
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            var url = _context.Urls.Find(id);
+
+            if (url == null)
+                return NotFound();
+
+            _context.Urls.Remove(url);
+            _context.SaveChanges();
+
             return Ok();
         }
     }
---
+++ UrlShortener/Dto/UrlDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Dto
+{
+    public class UrlDto
+    {
+        [Required]
+        public string Redirect { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
